65718a5feb000eaeac5831a18d2d3c59cda94472
unknown <coder.rakibul@gmail.com>
2022-06-10 02:27:49 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Services from './pages/Services/Services';
+import Footer from './pages/Shared/Footer';
 import Header from './pages/Shared/Header';
 
 function App() {
@@ -7,8 +13,14 @@ function App() {
     <div>
       <Header></Header>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/services" element={<Services></Services>}></Route>
+        <Route path="/projects" element={<Projects></Projects>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
       </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import Logo from '../../images/logo.png';
 
 
 const Header = () => {
