8c7d6d3f594455f2723470c5661ec62c1b37e8e8
unknown <coder.rakibul@gmail.com>
2022-06-13 23:34:37 +0600

    hire me updated

--- src/pages/Home/Hire.js
+++ src/pages/Home/Hire.js
@@ -1,10 +1,45 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
 const Hire = () => {
     return (
         <section className='container mt-5 mb-5'>
-            <h2 className='text-center'>Hire me</h2>
-            <div>
+            <h2 className='text-center mb-4'>Hire Me</h2>
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+
+                <div className="col">
+                    <div className="card h-100 bg-light">
+
+                        <div className="card-body text-center">
+                        <FontAwesomeIcon className='text-color fw-bold fs-1 mb-2' icon={faClock} />
+                            <h5 className="card-title">Hourly Rate</h5>
+                            <h4 className="card-text fw-bold">5$</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card h-100 bg-light">
+
+                        <div className="card-body text-center">
+                        <FontAwesomeIcon className='text-color fw-bold fs-1 mb-2' icon={faClock} />
+                            <h5 className="card-title">Per Day</h5>
+                            <h4 className="card-text fw-bold">50$</h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col">
+                    <div className="card h-100 bg-light">
+
+                    <div className="card-body text-center">
+                    <FontAwesomeIcon className='text-color fw-bold fs-1 mb-2' icon={faClock} />
+                            <h5 className="card-title">Per Month</h5>
+                            <h4 className="card-text fw-bold">1200$</h4>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </section>
