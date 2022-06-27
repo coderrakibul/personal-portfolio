bb5b536f574d5387295e24f777791d8220b2f954
unknown <coder.rakibul@gmail.com>
2022-06-28 01:00:08 +0600

    site update

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -15,10 +15,10 @@ const About = () => {
                         Hi! I'm Rakibul Islam. I'm a <strong className="text-color">React Front-End</strong> Web
                         Developer.
                     </h3>
-                    <h5> I am an enthusiastic Web Developer, having expertise in web application development, javascript problem solving, server side development and mobile app development. But highly interested in Client Side web development.
+                    <h5> I am an enthusiastic Web Developer, having expertise in web application development, javascript problem solving, server-side development, and mobile app development. But highly interested in Client-Side web development.
                         <br /><br />
 
-                        The creation of this portfolio provided me with a chance to reflect on both my technical skills and my professional skills in several years. As you browse around, I hope that you are able to gain a strong sense about me. Each section of this portfolio represents a small piece of information that reflects who I am and what I am capable of.</h5>
+                        The creation of this portfolio provided me with a chance to reflect on both my technical skills and my professional skills for several years. As you browse around, I hope that you are able to gain a strong sense of me. Each section of this portfolio represents a small piece of information that reflects who I am and what I am capable of.</h5>
                 </div>
             </div>
         </section>
